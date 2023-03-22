6f86a68c7863add9d5c2e0c72d51bb5fa4ada947
Khaliullin Ruslan <118468950+KhalioullinRuslan@users.noreply.github.com>
2023-03-22 21:06:07 +0300

    Update Electronic_Shop.java

--- Electronic_Shop.java
+++ Electronic_Shop.java
@@ -1,31 +1,10 @@
-// Подумать над структурой класса Ноутбук для магазина техники - выделить поля и методы. Реализовать в java.
-// Создать множество ноутбуков.
-// Написать метод, который будет запрашивать у пользователя критерий фильтрации и выведет ноутбуки, отвечающие фильтру.
-// NoteBook notebook1 = new NoteBook
-// NoteBook notebook2 = new NoteBook
-// NoteBook notebook3 = new NoteBook
-// NoteBook notebook4 = new NoteBook
-// NoteBook notebook5 = new NoteBook
-
-// Например: “Введите цифру, соответствующую необходимому критерию:
-// 1 - ОЗУ
-// 2 - Объем ЖД
-// 3 - Операционная система
-// 4 - Цвет
-
-// Далее нужно запросить минимальные значения для указанных критериев - сохранить параметры фильтрации можно также в Map.
-
-// Отфильтровать ноутбуки их первоначального множества и вывести проходящие по условиям.
-
-// Класс сделать в отдельном файле
-
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.TreeSet;
 
 
-// package Java_HomeWork_6;
+package Java_HomeWork_6;
 
 public class Electronic_Shop {
     public static void main(String[] args) {
